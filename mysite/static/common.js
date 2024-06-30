735b76c8d664b9c475aa9ed6b2a0986505446509
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2024-06-30 10:06:14 +0900

    designing portfolio

--- mysite/static/common.js
+++ mysite/static/common.js
@@ -1,27 +1,30 @@
-var url = new URL(window.location.href)
+var url = new URL(window.location.href);
 var windowWidth = $(window).width();
-$(document).ready(function(){
+$(document).ready(function () {
   console.log(url);
-  if (windowWidth < 768){
-  $(".left-sidebar").hide();}
-  else{
-    $(".left-sidebar").show();
+  if (windowWidth < 768) {
+    $('.left-sidebar').hide();
+  } else {
+    $('.left-sidebar').show();
   }
-  if(url.pathname == "/"){
-    $(".logo").hide();
+  if (url.pathname == '/') {
+    $('.logo').hide();
   }
-  $("#ham-button").off().on("click",function (e) {
-    $(".main-nav").toggle();
-  });
+  $('#ham-button')
+    .off()
+    .on('click', function (e) {
+      $('.main-nav').toggle();
+    });
 });
 
-$( window ).resize(function() {
+$(window).resize(function () {
   var windowWidth = $(window).width();
-  if (windowWidth < 768){
-    $("#trigger").prop('checked',false);
-    $(".left-sidebar").hide()
-  }else{
-      $(".left-sidebar").show();
-    $(".main-nav").hide();
+  if (windowWidth < 768) {
+    $('#trigger').prop('checked', false);
+    $('.main-nav').hide();
+    $('.left-sidebar').hide();
+  } else {
+    $('.left-sidebar').show();
+    $('.main-nav').hide();
   }
 });
